4966ac8fb56c0c269fd1265a6f5501fc50c80b07
Shubham <shubhampatilsd@gmail.com>
2023-03-07 09:21:19 -0800

    basic stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [hunger, setHunger] = useState<number>(40);
+  const [comfort, setComfort] = useState<number>(60);
+  const [happy, setHappy] = useState<number>(30);
+
   return (
     <>
       <Head>
@@ -14,110 +19,50 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main
+        className={
+          "rounded-lg bg-black p-6  max-h-[600px] max-w-[400px] shadow-lg"
+        }
+      >
+        <div
+          style={{
+            backgroundImage: "url(/background.png)",
+            height: "300px",
+            width: "300px",
+            backgroundSize: "contain",
+          }}
+          className="bg-contain mx-auto relative"
+        >
+          <div className="bg-white opacity-80 px-3">
+            <span className="w-full flex space-x-3 items-center">
+              <p>Hunger</p>
+              <div
+                className={`w-full rounded-full h-1 bg-red-500 border w-${hunger}% border-white`}
+              ></div>
+            </span>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+            <span></span>
+
+            <span></span>
+          </div>
+          <img
+            src="/character.png"
+            className="h-[100px] absolute mx-auto bottom-5 left-[30%]"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="space-y-4 mt-6">
+          <button className="border-2 hover:bg-yellow-500 hover:border-yellow-100 rounded-md border-yellow-500 bg-yellow-100 text-xl w-full">
+            Feed{" "}
+          </button>
+          <button className="border-2 hover:bg-yellow-500 hover:border-yellow-100  rounded-md border-yellow-500 bg-yellow-100 text-xl w-full">
+            Pet{" "}
+          </button>
+          <button className="border-2 hover:bg-yellow-500 hover:border-yellow-100  rounded-md border-yellow-500 bg-yellow-100 text-xl w-full">
+            Play{" "}
+          </button>
         </div>
       </main>
     </>
-  )
+  );
 }
